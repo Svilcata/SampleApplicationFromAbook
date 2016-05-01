4f23473090447fe909f7287d6eb494bb7598077e
Svilcata <lord_solen@abv.bg>
2016-05-01 13:07:50 +0300

    Chapter 2 : Complete

--- app/src/main/java/com/svilcata/sampleapplicationfromabook/MainActivity.java
+++ app/src/main/java/com/svilcata/sampleapplicationfromabook/MainActivity.java
@@ -2,6 +2,9 @@ package com.svilcata.sampleapplicationfromabook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.transition.TransitionManager;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewGroup = (ViewGroup) findViewById(R.id.view_group);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Animation demo");
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(viewGroup, new Explode());
+                toggle(textView, imageView, imageView2, imageView3);
+            }
+        });
+    }
+
+    private static void toggle(View... views) {
+        for (View v : views) {
+            boolean isVisible = v.getVisibility() == View.VISIBLE;
+            v.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
+        }
     }
 }
